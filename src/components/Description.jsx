9b1a67be9953e78470396eedc5ca157df5bb8710
alivetour <95938059+alivetour@users.noreply.github.com>
2022-04-04 13:32:19 -0700

    Update Description.jsx

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
 class Description extends React.Component {
     render() {
         return (
             <section className="container description-content">
                 <p className="content-copy">
-                    {this.props.copy}
+                    <ReactMarkdown>{this.props.copy}</ReactMarkdown>
                 </p>
             </section>
         )
